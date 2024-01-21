a30215ee3ac03f5bb44d6c34cadb2556f8088032
gabicota16 <134754166+gabicota16@users.noreply.github.com>
2024-01-21 17:28:28 -0300

    Update config.js

--- config.js
+++ config.js
@@ -3,12 +3,12 @@ import chalk from 'chalk'
 import { fileURLToPath } from 'url'
 
 global.owner = [
-  ['59172945992', 'FG98', true],
+  ['5493491456806', 'FG98', true],
   ['5491168352204']
 ] //Numeros de owner 
 
 global.mods = [''] 
-global.prems = ['50489079501', '573143917092']
+global.prems = ['5493491456806', '5493491456806']
 global.APIs = { // API Prefix
   // name: 'https://website' 
   nrtm: 'https://fg-nrtm.ddns.net',
